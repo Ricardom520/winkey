65aa39ba920388d70c3e47d524742974be39cba9
tanridong <tanridong@yy.com>
2022-10-17 20:20:43 +0800

    test: 备份

--- src/lib/formatter.ts
+++ src/lib/formatter.ts
@@ -1,15 +1,15 @@
-const YY_SEED_FULL_PREFIX = '@winkey/init-project-seed-'
-const SEED_FULL_PREFIX = 'init-project-seed-'
-const YY_SEED_SHORT_PREFIX = '@winkey/'
+const WINKEY_SEED_FULL_PREFIX = 'winkey-init-project-seed-'
+const SEED_FULL_PREFIX = 'winkey-init-project-seed-'
+const WINKEY_SEED_SHORT_PREFIX = 'winkey-'
 
 const REG = {
-  YY_SEED_FULL_PREFIX: new RegExp(`^${YY_SEED_FULL_PREFIX}`),
-  YY_SEED_SHORT_PREFIX: new RegExp(`^${YY_SEED_SHORT_PREFIX}`),
+  WINKEY_SEED_FULL_PREFIX: new RegExp(`^${WINKEY_SEED_FULL_PREFIX}`),
+  WINKEY_SEED_SHORT_PREFIX: new RegExp(`^${WINKEY_SEED_FULL_PREFIX}`),
   SEED_FULL_PREFIX: new RegExp(`^${SEED_FULL_PREFIX}`)
 }
 
 export const seedFull2Short = (name) => {
   return name
-    .replace(REG.YY_SEED_FULL_PREFIX, YY_SEED_SHORT_PREFIX)
+    .replace(REG.WINKEY_SEED_FULL_PREFIX, WINKEY_SEED_SHORT_PREFIX)
     .replace(REG.SEED_FULL_PREFIX, '')
 }
